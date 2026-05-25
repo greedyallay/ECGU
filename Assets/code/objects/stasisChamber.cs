a59b243a741443830d03d765ae72d1049bee8c21
Axolay <147523537+GreedyAllay@users.noreply.github.com>
2026-05-25 21:20:23 +0200

    why was it so hard to turn some lights off geez

--- Assets/code/objects/stasisChamber.cs
+++ Assets/code/objects/stasisChamber.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class stasisChamberHandler : MonoBehaviour
 {
+    public bool isEnabled = true;
+
+    private Light2D light1;
+    private Light2D light2;
+
+    public AudioClip onSound;
+    private AudioSource sound;
+
+    private bool previousState;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,19 @@ public class stasisChamberHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(light1 == null) {
+            light1  = transform.Find("light1").GetComponent<Light2D>();
+            light2  = transform.Find("light2").GetComponent<Light2D>();
+            sound  = transform.Find("sound").GetComponent<AudioSource>();
+            previousState = false;
+        }
+
+        if(isEnabled != previousState) {
+            light1.enabled = isEnabled;
+            light2.enabled = isEnabled;
+            sound.PlayOneShot(onSound);
+            previousState = isEnabled;
+        }
+
     }
 }
